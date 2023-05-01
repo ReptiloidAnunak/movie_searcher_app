e742a961e8acc1d4f78f99ba3c05e43c508c0e8b
ReptiloidAnunak <120068158+ReptiloidAnunak@users.noreply.github.com>
2023-04-30 21:28:37 -0300

    Delete termwork_4 directory

--- termwork_4/config.py
+++
@@ -1,8 +0,0 @@
-class Config(object):
-    DEBUG = True
-    JWT_SECRET = 'nopasanada'
-    JWT_ALGO = 'HS256'
-    PWD_SALT = b'bol7do'
-    PWD_ITERATIONS = 100000
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///./project.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
